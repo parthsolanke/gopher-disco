8b245260d1f22b448b31e7dc07090971a2375b94
parthsolanke <parthsolanke2002@gmail.com>
2025-08-03 19:42:41 +0530

    chore: add intresting take on go slices

--- playground/slices/main.go
+++ playground/slices/main.go
@@ -22,3 +22,23 @@ func printHeader(name string, s []int) {
 	fmt.Printf("%s -> slice: %v, addr: %p, len: %d, cap: %d\n", name, s, ptr, len(s), cap(s))
 	fmt.Println()
 }
+
+// Out:
+//~/dev/gopher-disco/playground/slices (main ✔) go run main.go
+//s1 -> slice: [1 2 3 4 5 6 7 8], addr: 0xc00001a180, len: 8, cap: 8
+//
+//s2 -> slice: [1 2], addr: 0xc00001a180, len: 2, cap: 8
+//
+//reassigned s1 -> slice: [2 3 4], addr: 0xc00001a188, len: 3, cap: 7
+//
+//[2 3 4 5 6 7 8]
+//~/dev/gopher-disco/playground/slices (main ✗)
+
+// practice to avoid changing the og array:
+//original := []int{1, 2, 3, 4, 5}
+//copySlice := make([]int, 3)
+//copy(copySlice, original[1:4]) // Copy [2, 3, 4] into new slice
+//
+//copySlice[0] = 99
+//fmt.Println(original)   // [1, 2, 3, 4, 5]
+//fmt.Println(copySlice)  // [99, 3, 4]
